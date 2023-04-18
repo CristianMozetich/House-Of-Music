c747c8c15b940d1b38e0faff7ab840d56ac14970
Cristian Mozetich <cristianmozetich@gmail.com>
2023-04-18 21:36:47 +0100

    pf1

--- assets/js/renderDrums.js
+++ assets/js/renderDrums.js
@@ -3,8 +3,8 @@ function renderDrums (){
     let salida = "";
 
     drums.forEach((drums) => {
-        salida +=  `<div class="card text-center border-secondary m-3 p-2 rounded" style="width: 18rem;">
-                            <img src="${drums.img}" class="card-img-top p-0" alt="drums">
+        salida +=  `<div class="card text-center border-secondary m-3 p-2" style="width: 18rem;">
+                            <img src="${drums.img}" class="card-img-top" alt="drums">
                             <div class="card-body">
                             <h5 class="card-title">${drums.name} </h5>
                             <h4 class="card-text p-2">€${drums.price}</h4>
